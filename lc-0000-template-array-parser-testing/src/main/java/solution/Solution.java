89011a12c5d76e0bdab4215716e431cb8f000af0
suad.bakhshiyev <122234216+SuadBakhshiyev@users.noreply.github.com>
2023-08-11 14:42:43 -0600

    Initialize jUnit array parser project.

---
+++ lc-0000-template-array-parser-testing/src/main/java/solution/Solution.java
@@ -0,0 +1,11 @@
+package solution;
+
+public class Solution {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("A test project to test jUnit array parser.");
+    }
+}
