e07c7afd0e77299b10472455a1f08ac0b40a4289
suad.bakhshiyev <122234216+SuadBakhshiyev@users.noreply.github.com>
2023-08-11 14:49:16 -0600

    Add 'add()' method to sum values of equal length arrays and output as new array.

--- lc-0000-template-array-parser-testing/src/main/java/solution/Solution.java
+++ lc-0000-template-array-parser-testing/src/main/java/solution/Solution.java
@@ -1,8 +1,16 @@
 package solution;
 
 public class Solution {
-    public int add(int a, int b) {
-        return a + b;
+    public int[] add(int[] nums1, int[] nums2) {
+        if (nums1.length != nums2.length) {
+            return new int[] {};
+        } else {
+            int ans[] = new int[nums1.length];
+            for (int i = 0; i < ans.length; i++) {
+                ans[i] = nums1[i] + nums2[i];
+            }
+            return ans;
+        }
     }
 
     public static void main(String[] args) {
