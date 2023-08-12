53c29efdcf94a9e10cf3a096ef5864dadcc99d2f
Nekotuko <132865851+nekotuko@users.noreply.github.com>
2023-08-11 21:19:07 -0700

    Example custom CsvSource arguments

--- lc-0000-template/src/main/java/solution/Solution.java
+++ lc-0000-template/src/main/java/solution/Solution.java
@@ -5,6 +5,15 @@ public class Solution {
         return a + b;
     }
 
+    public int[] addAll(int[] a, int[] b) {
+        int n = Math.min(a.length, b.length);
+        int[] c = new int[n];
+        for (int i = 0; i < n; ++i) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
     public static void main(String[] args) {
         System.out.println("I'm a template!");
     }
