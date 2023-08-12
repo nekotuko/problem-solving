08ecf9c9853965afb40c4492a6295b60cd1f9d48
Nekotuko <132865851+nekotuko@users.noreply.github.com>
2023-08-12 13:52:19 -0700

    Fix Gradle not showing in VS Code

--- buildSrc/src/main/kotlin/java-solution.gradle.kts
+++ buildSrc/src/main/kotlin/java-solution.gradle.kts
@@ -7,11 +7,8 @@ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
 
 dependencies {
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:31.1-jre")
 }
 
 application {
